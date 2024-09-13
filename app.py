49ae6749fd838ea4a1c5a44ec9f43cdca4bd1dbb
Hipolio <115099287+Hipolio@users.noreply.github.com>
2024-09-12 20:40:35 -0500

    Update app.py

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ def alumnosGuardar():
 @app.route("/evento", methods=["GET"])
 def (evento):
     pusher_client = pusher.Pusher(
-    app_id='1864235',
-    key='800476adc201b15ee59a',
-    secret='98a42826dd38896628f4',
-    cluster='us2',
-    ssl=True
-)
+        app_id='1864235',
+        key='800476adc201b15ee59a',
+        secret='98a42826dd38896628f4',
+        cluster='us2',
+        ssl=True
+    )
 
-pusher_client.trigger("conexion", "evento", request.arg)
+    pusher_client.trigger("conexion", "evento", request.arg)
