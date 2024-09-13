785718fa5866fcfa05a05dddc5800294f453f314
Hipolio <115099287+Hipolio@users.noreply.github.com>
2024-09-12 19:16:46 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
+import pusher
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -18,3 +20,15 @@ def alumnosGuardar():
     matricula      = request.form["txtMatriculaFA"]
     nombreapellido = request.form["txtNombreApellidoFA"]
     return f"Matrícula: {matricula} Nombre y Apellido: {nombreapellido}"
+
+@app.route("/evento")
+def (evento):
+    pusher_client = pusher.Pusher(
+    app_id='1864235',
+    key='800476adc201b15ee59a',
+    secret='98a42826dd38896628f4',
+    cluster='us2',
+    ssl=True
+)
+
+pusher_client.trigger('my-channel', 'my-event', {'message': 'hello world'})
